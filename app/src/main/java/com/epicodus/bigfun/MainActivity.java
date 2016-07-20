0344bbdf0758dfceeeb416e75e4efc8c471252d7
Ryan Carlile <ryancarlile@gmail.com>
2016-07-20 16:27:38 -0700

    Deleted useless code to clean up main activity

--- app/src/main/java/com/epicodus/bigfun/MainActivity.java
+++ app/src/main/java/com/epicodus/bigfun/MainActivity.java
@@ -6,15 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,11 +20,7 @@ import com.epicodus.bigfun.models.UserEvents;
 import com.facebook.*;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.facebook.login.widget.ProfilePictureView;
-import com.facebook.share.Sharer;
-import com.facebook.share.widget.ShareDialog;
-//import com.firebase.client.Firebase;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -35,11 +28,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -53,22 +42,12 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends FragmentActivity implements View.OnClickListener{
     public static final String TAG = MainActivity.class.getSimpleName();
-//    private Firebase mRef = new Firebase("https://big-fun.firebaseio.com/");
-
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseAuth mAuth;
-//    private DatabaseReference mSavedEventReference;
-//    private ValueEventListener mSavedEventReferenceListener;
-
-    private static final String PERMISSION = "user_events";
-    private final String PENDING_ACTION_BUNDLE_KEY = "com.epicodus.bigfun:PendingAction";
-
     private ProfilePictureView profilePictureView;
     private TextView greeting;
-    private PendingAction pendingAction = PendingAction.NONE;
     private CallbackManager callbackManager;
     private ProfileTracker profileTracker;
-    private ShareDialog shareDialog;
 
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
     @Bind(R.id.addEvent) ImageButton mAddEvent;
@@ -77,45 +56,6 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     private EventsListAdapter mAdapter;
 
     public ArrayList<UserEvents> mEvents = new ArrayList<>();
-
-    private FacebookCallback<Sharer.Result> shareCallback = new FacebookCallback<Sharer.Result>() {
-        @Override
-        public void onCancel() {
-            Log.d("HelloFacebook", "Canceled");
-        }
-
-        @Override
-        public void onError(FacebookException error) {
-            Log.d("HelloFacebook", String.format("Error: %s", error.toString()));
-            String title = getString(R.string.error);
-            String alertMessage = error.getMessage();
-            showResult(title, alertMessage);
-        }
-
-        @Override
-        public void onSuccess(Sharer.Result result) {
-            Log.d("HelloFacebook", "Success!");
-            if (result.getPostId() != null) {
-                String title = getString(R.string.success);
-                String id = result.getPostId();
-                String alertMessage = getString(R.string.successfully_posted_post, id);
-                showResult(title, alertMessage);
-            }
-        }
-
-        private void showResult(String title, String alertMessage) {
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle(title)
-                    .setMessage(alertMessage)
-                    .setPositiveButton(R.string.ok, null)
-                    .show();
-        }
-    };
-
-
-    private enum PendingAction {
-        NONE,
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -133,8 +73,6 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-
-                        handlePendingAction();
                         updateUI();
                         handleFacebookAccessToken(loginResult.getAccessToken());
 
@@ -184,20 +122,11 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
                     @Override
                     public void onCancel() {
-                        if (pendingAction != PendingAction.NONE) {
-                            showAlert();
-                            pendingAction = PendingAction.NONE;
-                        }
                         updateUI();
                     }
 
                     @Override
                     public void onError(FacebookException exception) {
-                        if (pendingAction != PendingAction.NONE
-                                && exception instanceof FacebookAuthorizationException) {
-                            showAlert();
-                            pendingAction = PendingAction.NONE;
-                        }
                         updateUI();
                     }
 
@@ -210,18 +139,6 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                     }
                 });
 
-
-
-        shareDialog = new ShareDialog(this);
-        shareDialog.registerCallback(
-                callbackManager,
-                shareCallback);
-
-        if (savedInstanceState != null) {
-            String name = savedInstanceState.getString(PENDING_ACTION_BUNDLE_KEY);
-            pendingAction = PendingAction.valueOf(name);
-        }
-
         setContentView(R.layout.main);
         ButterKnife.bind(this);
         mAddEvent.setOnClickListener(this);
@@ -232,34 +149,11 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
             @Override
             protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
                 updateUI();
-                handlePendingAction();
             }
         };
 
         profilePictureView = (ProfilePictureView) findViewById(R.id.profilePicture);
         greeting = (TextView) findViewById(R.id.greeting);
-
-//        mSavedEventReference = FirebaseDatabase
-//                .getInstance()
-//                .getReference()
-//                .child(Constants.FIREBASE_CHILD_SAVED_EVENT);
-//
-//        mSavedEventReferenceListener = mSavedEventReference.addValueEventListener(new ValueEventListener() {
-//
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                for (DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
-//                    String savedEvent = locationSnapshot.getValue().toString();
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//
-//        });
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -311,7 +205,6 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString(PENDING_ACTION_BUNDLE_KEY, pendingAction.name());
     }
 
 
@@ -326,7 +219,6 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     protected void onDestroy() {
         super.onDestroy();
         profileTracker.stopTracking();
-//        mSavedEventReference.removeEventListener(mSavedEventReferenceListener);
     }
 
     private void updateUI() {
@@ -341,14 +233,6 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         }
     }
 
-    private void handlePendingAction() {
-        PendingAction previouslyPendingAction = pendingAction;
-        pendingAction = PendingAction.NONE;
-        switch (previouslyPendingAction) {
-            case NONE:
-                break;
-        }
-    }
     private void handleFacebookAccessToken(AccessToken token) {
         Log.d(TAG, "handleFacebookAccessToken:" + token);
 
@@ -359,9 +243,6 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
 
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
                             Log.w(TAG, "signInWithCredential", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
